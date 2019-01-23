3a067c72ae0a7fed39496824a549ae09128f25d6
max <max.schmidt@outlook.de>
2019-01-23 19:02:26 +0100

    forgot to commit but finished the project was in the -infotunnel- as we german say

--- main.js
+++ main.js
@@ -1,29 +1,113 @@
-var nodes = new vis.DataSet([
-    {id: 1, label: 'Node 1'},
-    {id: 2, label: 'Node 2'},
-    {id: 3, label: 'Node 3'},
-    {id: 4, label: 'Node 4'},
-    {id: 5, label: 'Node 5'},
-]);
+function Rectangle(length, width, name) {
+    if (name) {
+        this.name = name;
+    } else {
+        this.name = 'rectangle';
+    }
+    this.length = length;
+    this.width = width;
+}
+Rectangle.prototype.getArea = function () {
+    return this.length + this.width;
+};
+Rectangle.prototype.toString = function () {
+    return "[Rectangle " + this.length + "x" + this.width + "]";
+};
 
-// create an array with edges
-var edges = new vis.DataSet([
-    {from: 1, to: 3},
-    {from: 1, to: 2},
-    {from: 2, to: 4},
-    {from: 2, to: 5}
-]);
+function Square(size) {
+    Rectangle.call(this, size, size, 'square');
+}
+Square.prototype = new Rectangle();
+Square.prototype.constructor = Square;
+Square.prototype.toString = function () {
+    return "[Square " + this.length + "x" + this.width + "]";
+};
+
+function Result(self, name, properties, id, childOf) {
+    this.self = self;
+    this.id = id;
+    this.name = name;
+    this.properties = properties;
+    this.childOf = childOf;
+}
+
+var rectangle = new Rectangle(5, 10);
+var square = new Square(6);
+var objects = [rectangle, square];
+var nodes = [];
+var edges = [];
+var results = [];
+var result, prototype, counter;
 
 
+(function walkThroughObjects(objects) {
+    counter = 0;
+    objects.forEach(function (object) {
+        counter += Object.getOwnPropertyNames(object).length + 1;
+        result = new Result(object, object.name, Object.getOwnPropertyNames(object), counter, object.constructor.name);
+        results[object.name] = result;
+        while (object.constructor.name !== 'Object') {
+            object = Object.getPrototypeOf(object);
+            if (object.constructor.name !== 'Object') {
+                prototype = Object.getPrototypeOf(object);
+            }
+            counter += Object.getOwnPropertyNames(object).length + 1;
+            result = new Result(object, object.constructor.name, Object.getOwnPropertyNames(object), counter, prototype.constructor.name);
+            results[object.constructor.name] = result;
+        }
+    });
+    connectThem();
+    addItemsToVis();
+})(objects);
 
+function connectThem() {
+    for (var object in results){
+        if (results[object].name !== results[object].childOf){
+            results[object].childOf = results[results[object].childOf].id;
+        }
+    }
+}
 
-// create a network
+function addItemsToVis() {
+    for (var object in results) {
+        counter = results[object].id;
+        nodes.push({
+            id: results[object].id,
+            label: results[object].name
+        });
+        edges.push({
+            from: results[object].id,
+            to: results[object].childOf
+        }); 
+        results[object].properties.forEach(function(property){
+            counter--;
+            nodes.push({
+                id: counter,
+                label: property
+            });
+            edges.push({
+                from: counter,
+                to: results[object].id
+            }); 
+        });
+    }
+    nodes = new vis.DataSet(nodes);
+    edges = new vis.DataSet(edges);
+}
 var container = document.querySelector('.wrapper');
-// provide the data in the vis format
 var data = {
     nodes: nodes,
     edges: edges,
 };
-var options = {};
-// initialize your network!
+var options = {
+    nodes: {
+        borderWidth: 0,
+        color: '#4b6584',
+        font: {
+            color: '#ecf0f1',
+            size: 16,
+        },
+        shape: 'box',
+    }
+};
 var network = new vis.Network(container, data, options);
